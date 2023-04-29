d6292f003f1828627df4282fddc304962ca7dbda
Barretotavio <obarretomachado@gmail.com>
2023-04-29 10:13:26 -0300

    started tittle styles

--- pokemons/src/Main/index.js
+++ pokemons/src/Main/index.js
@@ -3,7 +3,21 @@ import './styles.css';
 function Main() {
   return (
     <div className="container-main">
-      <h1>Pokemon</h1>
+      <div className='container-titulo'>
+        <h1>Pokedex</h1>
+      </div>
+      <section>
+        <ol className='container-pokemons'>
+          <div className='container-pares-pokemons'>
+            <li>Bulbasaur</li>
+            <li>Ivysaur</li>
+          </div>
+          <div>
+            <li>Venusaur</li>
+            <li>Charmander</li>
+          </div>
+        </ol>
+      </section>
     </div>
   );
 }
--- pokemons/src/routes.js
+++ pokemons/src/routes.js
@@ -1,1 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import Main from "./Main";
+
+function MainRoutes() {
+
+    return (
+        <Routes>
+            <Route path="/" element={<Main />} />
+        </Routes>
+    )
+}
+export default MainRoutes;
